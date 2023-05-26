26296867a230e1e8f71f18d8dd0d6c47c9b9077f
Dilshan Theekshana <theekshanajayarathne@gmail.com>
2023-05-26 23:15:27 +0530

    services completed

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -1,26 +1,42 @@
 const Service = require("../models/service.model");
+const cloudinary = require("../lib/cloudinary");
+const image = "not found";
 
 exports.create = async (req, res) => {
-  if (!req.body) {
-    res.status(400).send({
-      message: "Content can not be empty!",
-    });
-  }
-
-  const service = new Service({
-    title: req.body.title,
-  });
-
-  Service.create(service, (err, data) => {
-    if (err)
-      res.status(500).send({
-        code: 500,
-        success: false,
-        message:
-          err.message || "Some error occurred while creating the Service.",
+  try {
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!",
       });
-    else res.send(data);
-  });
+    }
+    let result;
+    if (req.file) {
+      result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "service_App_Pictures",
+      });
+    }
+
+    const service = new Service({
+      serviceType: req.body.serviceType,
+      serviceName: req.body.serviceName,
+      image: result?.secure_url || image,
+    });
+
+    Service.create(service, (err, data) => {
+      if (err)
+        res.status(500).send({
+          code: 500,
+          success: false,
+          message:
+            err.message || "Some error occurred while creating the Service.",
+        });
+      else res.send(data);
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ code: 500, success: false, message: "Internal Server Error" });
+  }
 };
 
 exports.findAll = async (req, res) => {
@@ -59,27 +75,36 @@ exports.findOne = async (req, res) => {
   }
 };
 
-exports.update = async (req, res) => {
+exports.update = async function (req, res) {
   try {
-    const service = await Service.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-      },
-      { new: true }
-    );
+    let service = await Service.findById(req.params.id);
+    let result;
+
+    if (req.file) {
+      result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "service_App_Pictures",
+      });
+    }
+
+    const data = {
+      serviceType: req.body.serviceType || service.serviceType,
+      serviceName: req.body.serviceName || service.serviceName,
+      image: result?.secure_url || service.image,
+    };
+
+    service = await Service.findByIdAndUpdate(req.params.id, data, {
+      new: true,
+    });
     res.status(200).json({
       code: 200,
       success: true,
-      users: service,
-      message: "Service updated successfully",
+      data: service,
+      message: "Service Updated Successfully!",
     });
-  } catch (error) {
-    res.status(500).json({
-      code: 500,
-      success: false,
-      message: "Internal Server Error",
-    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ code: 500, success: false, message: "Internal Server Error" });
   }
 };
 
