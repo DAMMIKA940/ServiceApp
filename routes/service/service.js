f5d78090b189b8f707188eb4755353532d6707b5
Dilshan Theekshana <theekshanajayarathne@gmail.com>
2023-06-08 22:52:05 +0530

    services completed 06-08

--- routes/service/service.js
+++ routes/service/service.js
@@ -8,6 +8,7 @@ Routes.get('/all', serviceController.findAll);
 Routes.get('/getbyId/:id', serviceController.findOne);
 Routes.put('/update/:id', storage.single("image"),serviceController.update);
 Routes.delete('/delete/:id', serviceController.delete);
+Routes.get('/getbyServiceType/:type', serviceController.getbyServiceType);
 
 module.exports = Routes;
 
